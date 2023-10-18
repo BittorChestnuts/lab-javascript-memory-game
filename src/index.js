776cfa5a37437e40b2aa312e019a223073583ec5
Victor <victor.castanos@gmail.com>
2023-10-18 17:26:38 +0200

    lab not done

--- src/index.js
+++ src/index.js
@@ -45,6 +45,9 @@ window.addEventListener('load', (event) => {
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
       // TODO: write some code here
+      
+
+      
       console.log(`Card clicked: ${card}`);
     });
   });
--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,49 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
     // add the rest of the class properties here
+
+    this.shuffleCards()
   }
 
   shuffleCards() {
     // ... write your code here
+    if (!this.cards) {
+      return undefined
+    } 
+    let len = this.cards.length;
+    while (len > 0) {
+      len--;
+      let temp = this.cards[len];
+      let rdmInd = Math.floor(Math.random() * len);
+      this.cards[len] = this.cards[rdmInd];
+      this.cards[rdmInd] = temp;
+    }
+    return this.cards
+    
   }
-
+  
   checkIfPair(card1, card2) {
+
+    this.pairsClicked++
+    if (card1 === card2){
+      this.pairsGuessed++
+      return true
+    } else {
+
+      return false
+    }
+
+    
     // ... write your code here
   }
 
   checkIfFinished() {
+
+    return false
     // ... write your code here
   }
 }
